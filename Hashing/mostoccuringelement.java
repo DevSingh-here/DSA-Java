07ab0dddffc55cdbf088787ac81b87f554fafb0e
Maxi <singh.davik7@gmail.com>
2026-07-29 20:06:24 +0530

    Update mostoccuringelement.java

--- Hashing/mostoccuringelement.java
+++ Hashing/mostoccuringelement.java
@@ -18,5 +18,39 @@ class Solution {
             }
         }
         return el;
+        // alternative understandable approach:
+        /*
+         * class Solution {
+         * public int mostFrequentElement(int[] nums) {
+         * int n=nums.length;
+         * int len=0;
+         * //find the max element in array
+         * for(int i=0;i<n;i++){
+         * len=Math.max(nums[i],len);
+         * }
+         * //create hash array and store the value
+         * int[] hash=new int[len+1];
+         * for(int i=0;i<n;i++){
+         * hash[nums[i]]++;
+         * }
+         * //compare
+         * int max=0;
+         * int el=0;
+         * for(int i=0;i<hash.length;i++){
+         * if(hash[i]>max){
+         * max=hash[i];
+         * el=i;
+         * }
+         * else if(hash[i]==max){
+         * el=Math.min(el,i);
+         * }
+         * }
+         * return el;
+         * }
+         * }
+         * 
+         * 
+         * 
+         */
     }
 }
